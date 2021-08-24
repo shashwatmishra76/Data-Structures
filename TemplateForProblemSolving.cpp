caef8497e1c28ba8a4083b6865c95bda332db1d2
Shashwat Mishra <shashwatmishra76@gmail.com>
2021-08-24 19:52:19 +0530

    Create TemplateForProblemSolving.cpp

---
+++ TemplateForProblemSolving.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    const int mod = 1e9 + 7;
+    //code here
+   
+    //code here
+    return 0;
+}
